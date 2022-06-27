af3a79a6c4ff2d3629e30707122830dc43d597b1
Jobsidney <jobsidney67@gmail.com>
2022-06-27 11:55:00 +0300

    DOne with assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+class Cat{
+    constructor(name){
+        this.name = name;
+    }
+    speak(){
+        return`${this.name} says meow!`
+    }
+}
+class Dog{
+    constructor(name){
+        this.name = name;
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+class Bird{
+    constructor(name){
+        this.name = name;
+    }
+    speak(){
+        return `${this.name} says squawk!`
+    }
+}
+const bird=new Bird('Pablo')
+bird.speak()
+const bird2=new Bird('Mable')
+bird2.speak()
+const dog=new Dog('Ruffio')
+dog.speak()
+const cat=new Cat('sasha')
+cat.speak()
